4b30985f7341c54cfbc91dc9ac52fb6baef41385
Shefali Mandal <shefalimandal1999@gmail.com>
2023-04-28 23:30:54 +0545

    Movie Website

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Home from "./pages/Home";
+import SingleMovie from "./pages/SingleMovie";
+import Error from "./pages/Error";
+import { Routes, Route } from "react-router-dom";
+import "./App.css"
+import Navbar from './components/Navbar';
+import About from './pages/About';
+import Contact from './pages/Contact';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="movie/:id" element={<SingleMovie />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="*" element={<Error />} />
+      </Routes>
+    </>
+  )
 }
 
-export default App;
+export default App
